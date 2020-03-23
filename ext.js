caff0fe1c7ae83c0e7e55a3ec807352d458110a1
Zakkor <edward.partenie@gmail.com>
2020-03-23 18:11:51 +0200

    link context is actually link + page, selection context, regex validation, context validation

--- ext.js
+++ ext.js
@@ -1,3 +1,4 @@
+const validContexts = ['link', 'selection']
 let servers = null;
 
 // Get servers set in options
@@ -34,47 +35,103 @@ function fetchMenuData(servers) {
 }
 
 function buildContextMenus(server, menus) {
+  // Get all possible contexts for all our menus
+  let contexts = []
   for (const menu of menus) {
-    const parent = chrome.contextMenus.create({ title: server, contexts:  menu.contexts }) 
+    for (const ctx of menu.contexts) {
+      // Validate context
+      if (validContexts.indexOf(ctx) == -1) {
+        notify(`Invalid context "${ctx}"`, 'Extension will not create any context menus')
+        return
+      }
+
+      if (contexts.indexOf(ctx) != -1) {
+        continue
+      }
+
+      contexts.push(ctx)
+      // link also means page
+      if (ctx == 'link') {
+        contexts.push('page')
+      }
+    }
+  }
+
+  // Create root context menu for this server: is available for all possilbe contexts
+  const title = `Send to ${server}`
+  var parent = chrome.contextMenus.create({ title: title, contexts: contexts })
+
+  for (const menu of menus) {
+    const options = {
+      title: menu.name,
+      parentId: parent,
+    }
 
     for (const ctx of menu.contexts) {
-      for (const pattern of menu.patterns) {
-        const options = {
-          parentId: parent,
-          title: menu.name,
-          contexts: [ctx],
-          onclick: postAction(server, menu.action),
+      switch (ctx) {
+      case 'link':
+        for (const pattern of menu.patterns) {
+          // Add both link and page contexts
+          chrome.contextMenus.create({ 
+            ...options,
+            contexts: ['page'],
+            documentUrlPatterns: [pattern],
+            onclick: postAction(server, menu.action, menu.regexes, 'pageUrl')
+          })
+          chrome.contextMenus.create({ 
+            ...options,
+            contexts: ['link'],
+            targetUrlPatterns: [pattern],
+            onclick: postAction(server, menu.action, menu.regexes, 'linkUrl')
+          })
         }
+        break
 
-        if (ctx == 'page') {
-          options.documentUrlPatterns = [pattern]
-          options.onclick = postAction(server, menu.action, 'pageUrl')
-        } else if (ctx == 'link') {
-          options.targetUrlPatterns = [pattern]
-          options.onclick = postAction(server, menu.action, 'linkUrl')
-        }
-
-        chrome.contextMenus.create(options)
+      case 'selection':
+        chrome.contextMenus.create({ 
+          ...options,
+          contexts: ['selection'],
+          onclick: postAction(server, menu.action, menu.regexes, 'selectionText')
+        })
+        break
       }
     }
   }
 }
 
-function postAction(server, action, linkKey) {
+function postAction(server, action, regexes, dataKey) {
   return function(info, tab) {
-    const link = encodeURIComponent(info[linkKey])
-    const url = encodeURI(`${server}/action?action=${action}&link=${link}`)
+    const data = encodeURIComponent(info[dataKey])
+    const url = encodeURI(`${server}/action?action=${action}&data=${data}`)
+
+    if (regexes.length > 0) {
+      let matched = false
+      for (const regex of regexes) {
+        if (data.match(regex)) {
+          matched = true
+          break
+        }
+      }
+      if (!matched) {
+        notify('Action cannot be sent', 'Data does not match clicked action')
+        return
+      }
+    }
 
     fetch(url, { method: 'POST' })
       .then(res => res.text())
       .then(text => {
-        var browser = browser || chrome
-        browser.notifications.create('', {
-          "type": "basic",
-          "iconUrl": browser.runtime.getURL("icon32.png"),
-          "title": "Action complete",
-          "message": text,
-        })
+        notify('Action complete', text)
       })
   }
+}
+
+function notify(title, message) {
+  var browser = browser || chrome
+  browser.notifications.create('', {
+    "type": "basic",
+    "iconUrl": browser.runtime.getURL("icon32.png"),
+    "title": title,
+    "message": message,
+  })
 }
