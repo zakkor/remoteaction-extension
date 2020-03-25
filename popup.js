c1cbccdbf2bdc9230ed884de1261fba04ae991a4
zakkor <edward.partenie@gmail.com>
2020-03-25 17:22:32 +0200

    show refresh button

--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
+let saveBtn = document.getElementById('save')
+let serversArea = document.getElementById('servers')
+
+serversArea.onchange = function() {
+	saveBtn.textContent = 'Save'
+}
+
 // Saves options to chrome.storage
 function saveOptions() {
-  const servers = document.getElementById('servers').value
+  const servers = serversArea.value
 
   chrome.storage.sync.set({ servers: servers }, function() {
     chrome.runtime.reload()
     window.close()
   })
 }
-document.getElementById('save').addEventListener('click', saveOptions)
+saveBtn.addEventListener('click', saveOptions)
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
@@ -16,7 +23,13 @@ function restoreOptions() {
   chrome.storage.sync.get({
     servers: '',
   }, function(items) {
-    document.getElementById('servers').value = items.servers
+    serversArea.value = items.servers
+    // Set save button text to either "save" or refresh
+	if (serversArea.value == '') {
+		saveBtn.textContent = 'Save'
+	} else {
+		saveBtn.textContent = 'Refresh'
+	}
   })
 }
 document.addEventListener('DOMContentLoaded', restoreOptions)
